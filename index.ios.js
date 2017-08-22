a335f7932b6e541853ba6c664ccce8c88c2dc39d
Pulkit Aggarwal <pulkitagg1010@gmail.com>
2017-08-22 18:03:49 +0530

    Update index.ios.js

--- index.ios.js
+++ index.ios.js
@@ -1,53 +1,19 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+import React from 'react';
+import {AppRegistry} from 'react-native';
+import {StackNavigator} from 'react-navigation';
 
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import FirstTab from './components/FirstTab.js';
+import SecondTab from './components/SecondTab.js';
+import FirstScreen from './components/FirstScreen.js';
+import TabScreen from './components/TabScreen.js';
 
-export default class FirstProject extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+const FirstProject = StackNavigator({
+     Home: {
+          screen: FirstScreen
+     },
+     Tabs: {
+          screen: TabScreen
+     }
 });
 
 AppRegistry.registerComponent('FirstProject', () => FirstProject);
